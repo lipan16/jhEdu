b5ddd3a84c40ef826e225f30fb1ac187c8fc398d
lipan <lipan16@lzu.edu.cn>
2023-07-02 00:55:38 +0800

    feat: 调整样式

--- src/layout/components/DefaultHeader.vue
+++ src/layout/components/DefaultHeader.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex-inline w-full p-2 bg-cyan-200 overflow-auto">
+    <div class="flex-inline w-full px-4 py-2 bg-cyan-200 overflow-auto">
         <img class="w-1/4" src="@/assets/logo_font.png" alt=""/>
-        <div class="w-3/4 flex-inline">
-            <div class="ml-2 mr-2 w-max text-lg text-white hover:text-red-400 text-center"
+        <div class="flex-inline w-3/4 justify-end">
+            <div class="mx-2 w-max text-lg text-white hover:text-red-400 text-center"
                  v-for="(item, index) in nav"
                  :key="index"
                  @click="onClickNav(item)"
@@ -24,13 +24,7 @@ const nav = [
     {title: '校园服务', link: '/'}
 ]
 
-const onClickNav = (item) => {
+const onClickNav = (item: any) => {
     console.log(item)
 }
 </script>
-<style scoped lang="less">
-// todo 设配移动端
-@media screen and (max-width: 768px){
-
-}
-</style>
