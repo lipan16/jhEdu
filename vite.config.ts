6c4a5f024880d9962211a9308ef37d4773548447
lipan <lipan16@lzu.edu.cn>
2023-07-07 20:12:30 +0800

    feat: 添加build参数

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,26 @@ export default defineConfig(({command, mode}) => {
             postcss: {
                 plugins: [tailwindcss]
             }
+        },
+        build: {
+            minify: 'esbuild', // 必须开启：使用terserOptions才有效果
+            terserOptions: {
+                compress: {
+                    keep_infinity: true,
+                    // 生产环境时移除console
+                    drop_console: true,
+                    drop_debugger: true
+                }
+            },
+            assetsInlineLimit: 4096, // 小于此阈值的导入或引用资源将内联为 base64 编码
+            cssCodeSplit: true, // 启用 CSS 代码拆分
+            cssTarget: '', // 允许用户为 CSS 的压缩设置一个不同的浏览器 target 与 build.target 一致
+            sourcemap: false, // 构建后是否生成 source map 文件
+            manifest: false, // 当设置为 true，构建后将会生成 manifest.json 文件
+            ssrManifest: false, // 构建不生成 SSR 的 manifest 文件
+            emptyOutDir: true, // 构建时清空该目录
+            brotliSize: true, // 启用 brotli 压缩大小报告
+            chunkSizeWarningLimit: 500, // chunk 大小警告的限制
         }
     }
 })
